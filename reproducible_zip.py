772f569c608d885e43369582019366429338df3a
Saleem Rashid <spudowiar@outlook.com>
2016-04-30 19:33:13 +0100

    exploit: improve image generation

--- reproducible_zip.py
+++ reproducible_zip.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 import os, sys, mmap
 from zipfile import ZipFile, ZipInfo
 
@@ -6,6 +7,9 @@ with ZipFile(sys.argv[1], "w") as z:
         with open(filename, "r+b") as f:
             mm = mmap.mmap(f.fileno(), 0)
             try:
-                z.writestr(ZipInfo(os.path.basename(filename), (1980, 1, 1, 0, 0, 0)), mm)
+                info = ZipInfo(os.path.basename(filename), (1980, 1, 1, 0, 0, 0))
+                info.create_system = 3 # Unix
+                info.external_attr = 0644 << 16L # rw-r--r--
+                z.writestr(info, mm)
             finally:
                 mm.close()
